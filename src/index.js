90c3103eec7dadad17ea0248fa15c3d90d60f5bd
Andriukh Nikita <nandriukh@allmax.team>
2019-01-23 09:39:15 +0700

    Changes3

--- src/index.js
+++ src/index.js
@@ -188,7 +188,7 @@ export default class RNPickerSelect extends PureComponent {
         onValueChange(value, index);
 
         this.setState({
-          selectedItem: this.state.items[index],
+            selectedItem: this.state.items[index],
         });
     }
 
@@ -279,15 +279,6 @@ export default class RNPickerSelect extends PureComponent {
         );
     });
 
-    onCancel = () => {
-      const { onCancelPress } = this.props;
-      onCancelPress();
-      this.setState(
-        { selectedItem: {  } },
-        this.animatedParallel(0, defaultScale)
-        )
-    }
-
     renderDoneBar = () => {
       const { doneText, hideDoneBar, onUpArrow, onDownArrow, onDonePress, style, onCancelPress, isFocused } = this.props;
 
@@ -332,7 +323,7 @@ export default class RNPickerSelect extends PureComponent {
                     <TouchableWithoutFeedback
                       onPress={() => {
                         this.togglePicker(true);
-                        this.onCancel()
+                        onCancelPress()
                       }}
                       hitSlop={{ top: 2, right: 2, bottom: 2, left: 2 }}
                       testID="cancel_button"
